c2907bc69bd6bb2b50ef722f028f237af8f696e4
asilaka <abduzalilovasilbek27@gmail.com>
2025-02-10 17:06:20 +0500

    Add files via upload

--- src/components/Wrap/Wrap.jsx
+++ src/components/Wrap/Wrap.jsx
@@ -1,35 +1,47 @@
-import React from 'react'
-import s from './Wrap.module.scss'
+import React, { useState } from 'react';
+import s from './Wrap.module.scss';
+
+const images = {
+  'One Piece': 'https://occ-0-8407-90.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABTYmlge88J4-0ouuHWDsP8VZBuoM_3FPfGZo-YMOixHeB1qduoBni9iahxi3zkEXNvwZ4Xez_JoRDiPq3hyLRyef43_fNRUJziyD.jpg?r=d90',
+  'Jujutsu Kaisen': 'https://static1.srcdn.com/wordpress/wp-content/uploads/2023/10/jujutsu-kaisen-s-main-cast.jpg',
+  'Naruto': 'narutos.jpg',
+};
+
 const Wrap = () => {
+  const [selectedImage, setSelectedImage] = useState(images['One Piece']);
+  const [activeButton, setActiveButton] = useState('One Piece');
+
   return (
-   <>
-<div className={s.nep}>
-    <div className="container">
-    <div className={s.title}>
-    <h1>Shop by Anime</h1>
-    <p>Lorem ipsum dolor sit amet, consectetuer adipi scing elit, sed diam nonummy nibh euismod tincidunt <br /> ut laoreet dolore magn. Lorem ipsum dolor sit amet, consectetuer adipi scing elit, sed diam nonummy</p>
-</div>
-   <div className={s.wrap}>
-
-
-    <img src="piece.png" alt="" />
-    <div className={s.naruto}>
-        <img src="/Rectangle 24589.png" alt="" />
-        <img src="/Rectangle 24590.png" alt="" />
-        <img src="/Rectangle 24591.png" alt="" />
-        <img src="/Rectangle 24592.png" alt="" />
-        <img src="/Rectangle 24593.png" alt="" />
-        <img src="/Rectangle 24594.png" alt="" />
+    <div className={s.nep}>
+      <div className="container">
+        <div className={s.title}>
+          <h1>Shop by Anime</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipi scing elit, sed diam nonummy nibh euismod
+            tincidunt <br /> ut laoreet dolore magn. Lorem ipsum dolor sit amet, consectetuer adipi
+            scing elit, sed diam nonummy
+          </p>
+        </div>
+        <div className={s.wrap}>
+          <div className={s.btns}>
+            {Object.keys(images).map((anime) => (
+              <button
+                key={anime}
+                onClick={() => {
+                  setSelectedImage(images[anime]);
+                  setActiveButton(anime);
+                }}
+                className={activeButton === anime ? s.activeButton : ''}
+              >
+                {anime}
+              </button>
+            ))}
+          </div>
+          <img src={selectedImage} alt="Anime" className={s.image} />
+        </div>
+      </div>
     </div>
-   </div>
+  );
+};
 
-    </div>
-
-    
-</div>
-
-    </>
-  )
-}
-
-export default Wrap
+export default Wrap;
